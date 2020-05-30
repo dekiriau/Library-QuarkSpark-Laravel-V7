a569f6ec8c2d0f20d5e2c1d3cb7f28bd83da88e5
Deki Riau Putra Bestari <deky.riau16@gmail.com>
2020-05-31 06:14:48 +0700

    copy controllers from previous project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laratrust\LaratrustFacade as Laratrust;
+use App\Author;
 
 class HomeController extends Controller
 {
@@ -19,15 +22,33 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
+        if (Laratrust::hasRole('admin')) return $this->adminDashboard();
+        if (Laratrust::hasRole('member')) return $this->memberDashboard();
+
         return view('home');
     }
 
-    public function adminHome()
+    protected function adminDashboard()
     {
-        return view('adminHome');
+        $authors = [];
+        $books = [];
+
+        foreach (Author::all() as $author) {
+            array_push($authors, $author->name);
+            array_push($books, $author->books->count());
+        }
+
+        return view('dashboard.admin', compact('authors', 'books'));
     }
+
+    protected function memberDashboard()
+    {
+        $borrowLogs = Auth::user()->borrowLogs()->borrowed()->get();
+        return view('dashboard.member', compact('borrowLogs'));
+    }
+
 }
