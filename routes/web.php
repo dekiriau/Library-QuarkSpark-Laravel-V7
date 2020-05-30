db2dfe4f7780bd4cfdffae2617898faae719c33d
Deki Riau Putra Bestari <deky.riau16@gmail.com>
2020-05-31 06:15:56 +0700

    copy web from previous project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,5 +20,62 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/', 'GuestController@index');
+
+
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('admin/home', 'HomeController@adminHome')->name('admin.home')->middleware('is_admin');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function() {
+    Route::resource('authors', 'AuthorsController');
+    Route::resource('books', 'BooksController');
+    Route::resource('members', 'MembersController');
+    Route::get('statistics', [
+        'as'    =>  'statistics.index',
+        'uses'  =>  'StatisticsController@index'
+    ]);
+
+    Route::get('export/books', [
+        'as' => 'export.books',
+        'uses' => 'BooksController@export'
+    ]);
+    Route::post('export/books', [
+        'as' => 'export.books.post',
+        'uses' => 'BooksController@exportPost'
+    ]);
+
+    Route::get('template/books', [
+        'as' => 'template.books',
+        'uses' => 'BooksController@generateExcelTemplate'
+    ]);
+    Route::post('import/books', [
+        'as' => 'import.books',
+        'uses' => 'BooksController@importExcel'
+    ]);
+
+    Route::post('members/{memberId}/status/toggle')
+        ->name('members.status.toggle')
+        ->uses('MembersController@statusToggle');
+});
+
+
+Route::get('books/{book}/borrow', [
+    'middleware' =>  ['auth', 'role:member'],
+    'as'         =>  'guest.books.borrow',
+    'uses'       =>  'BooksController@borrow'
+]);
+
+
+Route::put('books/{book}/return', [
+    'middleware' =>  ['auth', 'role:member'],
+    'as'         =>  'member.books.return',
+    'uses'       =>  'BooksController@returnBack'
+]);
+
+Route::get('auth/verify/{token}', 'Auth\RegisterController@verify');
+Route::get('auth/send-verification', 'Auth\RegisterController@sendVerification');
+
+Route::get('settings/profile', 'SettingsController@profile');
+Route::get('settings/profile/edit', 'SettingsController@editProfile');
+Route::post('settings/profile', 'SettingsController@updateProfile');
+Route::get('settings/password', 'SettingsController@editPassword');
+Route::post('settings/password', 'SettingsController@updatePassword');
