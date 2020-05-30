d8c29035bd0767698cd642928e0cf5a4a931b4bf
Deki <deky.riau16@gmail.com>
2020-05-31 02:04:39 +0700

    user model update

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'is_admin'
+        'name', 'email', 'password', 'is_admin', 'enabled'
     ];
 
     /**
@@ -35,5 +35,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'enabled' => 'boolean'
     ];
 }
